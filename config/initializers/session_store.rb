f79fea94f2c2932de7c79554b7d0a789881183cc
Guy <guymoustapha@gmail.com>
2021-06-03 10:51:02 +0200

    config middleware and cookies

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,4 @@
+if Rails.env === 'production' 
+  Rails.application.config.session_store :cookie_store, key: '_final', domain: 'final-json-api'
+else
+  Rails.application.config.session_store :cookie_store, key: '_final'
