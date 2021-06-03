6d88d8e776711d746b6870d7a33fe8cd9cdc02aa
Guy <guymoustapha@gmail.com>
2021-06-03 12:53:06 +0200

    add model user

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,3 +2,4 @@ if Rails.env === 'production'
   Rails.application.config.session_store :cookie_store, key: '_final', domain: 'final-json-api'
 else
   Rails.application.config.session_store :cookie_store, key: '_final'
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post '/users',         to: 'users#create'
+  get '/users/:user_id', to: 'users#show'
+  get '/users',          to: 'users#index'
   resources :projects do
     resources :tasks
   end
